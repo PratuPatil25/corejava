2568666c0d14c1a6f043b20d486070f77654429e
Pratiksha Patil <96839446+PratuPatil25@users.noreply.github.com>
2024-10-09 13:06:23 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,33 @@
+package data_type;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		
+		byte no1 = 25;
+		System.out.println(no1);
+		
+		short no2 = 5;
+		System.out.println("Short no: "+no2);
+		
+		int no3 = 10;
+		System.out.println("Int no is : " +no3);
+		
+		long no4 = 8664345664L;
+		System.out.println("Long no is :" +no4);
+		
+		String name = "Pratiksha";
+		System.out.println("My name is :"+ name);
+		
+		float no5 = 10.5f;
+		System.out.println("Decimal no: " +no5);
+		
+		double no6 = 10.6875876587987;
+		System.out.println("Double Decimal no :"+no6);
+		
+		boolean flag = true;
+		System.out.println("Boolean value is: "+flag);
+
+	}
+
+}
