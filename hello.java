ae727ab4eaf67c5c82dfb4d313e68922b8f8b61d
Pratiksha Patil <96839446+PratuPatil25@users.noreply.github.com>
2024-10-09 13:22:30 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+package java_first_package;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("om Namh shivay"); 
+	}
+
+}
