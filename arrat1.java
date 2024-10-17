219419f03e651a2e88fe505e454c17dda8beace6
Pratiksha Patil <96839446+PratuPatil25@users.noreply.github.com>
2024-10-17 16:42:16 +0530

    Add files via upload

---
+++ arrat1.java
@@ -0,0 +1,17 @@
+package array;
+
+public class arrat1 {
+	
+	public static void main(String[] args) {
+		
+		String animals[]= {"Dog","Cat","cow","Horse","sheep","pig","Duck","Fish","Rabbit","Tiger"};
+				
+	    
+	      for(int  i=0;i<animals.length;i++)
+	{
+		  System.out.println(animals[i]);
+	}
+
+	}
+
+}
